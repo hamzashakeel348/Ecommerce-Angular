ab5c41028eb28f4526790edf6bc8ff96d4f406e3
M Hamza Shakeel <55921857+hamzashakeel348@users.noreply.github.com>
2021-06-19 21:23:39 +0500

    Version2

--- src/app/components/feature/feature.component.ts
+++ src/app/components/feature/feature.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-feature',
-  templateUrl: './feature.component.html',
-  styleUrls: ['./feature.component.css']
+  selector: "app-feature",
+  templateUrl: "./feature.component.html",
+  styleUrls: ["./feature.component.css"],
 })
 export class FeatureComponent implements OnInit {
+  @Input() t1: string = "";
+  @Input() t2: string = "";
+  @Input() original: string = "";
+  @Input() discount: string = "";
+  @Input() url: string = "";
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
